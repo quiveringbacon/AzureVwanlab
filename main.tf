c8a96b43caa2efbcb782981b5fe74d322d222be0
quiveringbacon <128983862+quiveringbacon@users.noreply.github.com>
2023-04-12 13:55:37 -0500

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,329 @@
+provider "azurerm" {
+  features {
+  }
+}
+
+#variables
+variable "A-location" {
+    description = "Location of the resources"
+    #default     = "eastus"
+}
+
+variable "B-resource_group_name" {
+    description = "Name of the resource group to create"
+}
+
+variable "C-home_public_ip" {
+    description = "Your home public ip address"
+}
+
+variable "D-username" {
+    description = "Username for Virtual Machines"
+    #default     = "azureuser"
+}
+
+variable "E-password" {
+    description = "Password for Virtual Machines"
+    sensitive = true
+}
+
+resource "azurerm_resource_group" "RG" {
+  location = var.A-location
+  name     = var.B-resource_group_name
+}
+
+#vwan and hub
+resource "azurerm_virtual_wan" "vwan1" {
+  name                = "vwan1"
+  resource_group_name = azurerm_resource_group.RG.name
+  location            = azurerm_resource_group.RG.location
+    timeouts {
+    create = "1h"
+    read = "1h"
+    update = "1h"
+    delete = "1h"
+  }
+  
+}
+
+resource "azurerm_virtual_hub" "vhub1" {
+  name                = "vhub1"
+  resource_group_name = azurerm_resource_group.RG.name
+  location            = azurerm_resource_group.RG.location
+  virtual_wan_id      = azurerm_virtual_wan.vwan1.id
+  address_prefix      = "10.0.0.0/16"
+    timeouts {
+    create = "1h"
+    read = "1h"
+    update = "1h"
+    delete = "1h"
+  }
+  
+}
+
+
+
+#spoke vnets
+resource "azurerm_virtual_network" "spoke1-vnet" {
+  address_space       = ["10.150.0.0/16"]
+  location            = azurerm_resource_group.RG.location
+  name                = "spoke1-vnet"
+  resource_group_name = azurerm_resource_group.RG.name
+  subnet {
+    address_prefix     = "10.150.0.0/24"
+    name                 = "default"
+    security_group = azurerm_network_security_group.spokevnetNSG.id
+  }
+  subnet {
+    address_prefix     = "10.150.1.0/24"
+    name                 = "GatewaySubnet" 
+  }
+  timeouts {
+    create = "1h"
+    read = "1h"
+    update = "1h"
+    delete = "1h"
+  }
+  
+}
+resource "azurerm_virtual_network" "spoke2-vnet" {
+  address_space       = ["10.250.0.0/16"]
+  location            = azurerm_resource_group.RG.location
+  name                = "spoke2-vnet"
+  resource_group_name = azurerm_resource_group.RG.name
+  subnet {
+    address_prefix     = "10.250.0.0/24"
+    name                 = "default"
+    security_group = azurerm_network_security_group.spokevnetNSG.id
+  }
+  subnet {
+    address_prefix     = "10.250.1.0/24"
+    name                 = "GatewaySubnet" 
+  }
+  timeouts {
+    create = "1h"
+    read = "1h"
+    update = "1h"
+    delete = "1h"
+  }
+  
+}
+
+
+
+#vnet connections to hub
+resource "azurerm_virtual_hub_connection" "tospoke1" {
+  name                      = "tospoke1"
+  internet_security_enabled = true
+  virtual_hub_id            = azurerm_virtual_hub.vhub1.id
+  remote_virtual_network_id = azurerm_virtual_network.spoke1-vnet.id
+}
+resource "azurerm_virtual_hub_connection" "tospoke2" {
+  name                      = "tospoke2"
+  internet_security_enabled = true
+  virtual_hub_id            = azurerm_virtual_hub.vhub1.id
+  remote_virtual_network_id = azurerm_virtual_network.spoke2-vnet.id
+}
+
+#NSG
+resource "azurerm_network_security_group" "spokevnetNSG" {
+  location            = azurerm_resource_group.RG.location
+  name                = "spoke-vnet-default-nsg"
+  resource_group_name = azurerm_resource_group.RG.name
+  timeouts {
+    create = "1h"
+    read = "1h"
+    update = "1h"
+    delete = "1h"
+  }
+  
+}
+resource "azurerm_network_security_rule" "spokevnetnsgrule1" {
+  access                      = "Allow"
+  destination_address_prefix  = "*"
+  destination_port_range      = "3389"
+  direction                   = "Inbound"
+  name                        = "AllowCidrBlockRDPInbound"
+  network_security_group_name = "spoke-vnet-default-nsg"
+  priority                    = 2711
+  protocol                    = "Tcp"
+  resource_group_name         = azurerm_network_security_group.spokevnetNSG.resource_group_name
+  source_address_prefix       = var.C-home_public_ip
+  source_port_range           = "*"
+  timeouts {
+    create = "1h"
+    read = "1h"
+    update = "1h"
+    delete = "1h"
+  }
+  
+}
+
+
+#Public ip's
+resource "azurerm_public_ip" "spoke1vm-pip" {
+  name                = "spoke1vm-pip"
+  location            = azurerm_resource_group.RG.location
+  resource_group_name = azurerm_resource_group.RG.name
+  allocation_method = "Dynamic"
+  timeouts {
+    create = "1h"
+    read = "1h"
+    update = "1h"
+    delete = "1h"
+  }
+  
+}
+resource "azurerm_public_ip" "spoke2vm-pip" {
+  name                = "spoke2vm-pip"
+  location            = azurerm_resource_group.RG.location
+  resource_group_name = azurerm_resource_group.RG.name
+  allocation_method = "Dynamic"
+  timeouts {
+    create = "1h"
+    read = "1h"
+    update = "1h"
+    delete = "1h"
+  }
+  
+}
+
+
+
+#vnic's
+resource "azurerm_network_interface" "spoke1vm-nic" {
+  location            = azurerm_resource_group.RG.location
+  name                = "spoke1vm-nic"
+  resource_group_name = azurerm_resource_group.RG.name
+  ip_configuration {
+    name                          = "ipconfig1"
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.spoke1vm-pip.id
+    subnet_id                     = azurerm_virtual_network.spoke1-vnet.subnet.*.id[0]
+  }
+  timeouts {
+    create = "1h"
+    read = "1h"
+    update = "1h"
+    delete = "1h"
+  }
+  
+}
+resource "azurerm_network_interface" "spoke2vm-nic" {
+  location            = azurerm_resource_group.RG.location
+  name                = "spoke2vm-nic"
+  resource_group_name = azurerm_resource_group.RG.name
+  ip_configuration {
+    name                          = "ipconfig1"
+    private_ip_address_allocation = "Dynamic"
+    public_ip_address_id          = azurerm_public_ip.spoke2vm-pip.id
+    subnet_id                     = azurerm_virtual_network.spoke2-vnet.subnet.*.id[0]
+  }
+  timeouts {
+    create = "1h"
+    read = "1h"
+    update = "1h"
+    delete = "1h"
+  }
+  
+}
+
+#VM's
+resource "azurerm_windows_virtual_machine" "spoke1vm" {
+  admin_password        = var.E-password
+  admin_username        = var.D-username
+  location              = azurerm_resource_group.RG.location
+  name                  = "spoke1vm"
+  network_interface_ids = [azurerm_network_interface.spoke1vm-nic.id]
+  resource_group_name   = azurerm_resource_group.RG.name
+  size                  = "Standard_B2ms"
+  identity {
+    type = "SystemAssigned"
+  }
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Premium_LRS"
+  }
+  source_image_reference {
+    offer     = "WindowsServer"
+    publisher = "MicrosoftWindowsServer"
+    sku       = "2016-datacenter-gensecond"
+    version   = "latest"
+  }
+  timeouts {
+    create = "1h"
+    read = "1h"
+    update = "1h"
+    delete = "1h"
+  }
+  
+}
+resource "azurerm_virtual_machine_extension" "killspoke1vmfirewall" {
+  auto_upgrade_minor_version = true
+  name                       = "killspoke1vmfirewall"
+  publisher                  = "Microsoft.Compute"
+  type                       = "CustomScriptExtension"
+  type_handler_version       = "1.10"
+  virtual_machine_id         = azurerm_windows_virtual_machine.spoke1vm.id
+  settings = <<SETTINGS
+    {
+      "commandToExecute": "powershell -command \"Set-NetFirewallProfile -Enabled False\""
+    }
+  SETTINGS
+  timeouts {
+    create = "1h"
+    read = "1h"
+    update = "1h"
+    delete = "1h"
+  }
+  
+}
+resource "azurerm_windows_virtual_machine" "spoke2vm" {
+  admin_password        = var.E-password
+  admin_username        = var.D-username
+  location              = azurerm_resource_group.RG.location
+  name                  = "spoke2vm"
+  network_interface_ids = [azurerm_network_interface.spoke2vm-nic.id]
+  resource_group_name   = azurerm_resource_group.RG.name
+  size                  = "Standard_B2ms"
+  identity {
+    type = "SystemAssigned"
+  }
+  os_disk {
+    caching              = "ReadWrite"
+    storage_account_type = "Premium_LRS"
+  }
+  source_image_reference {
+    offer     = "WindowsServer"
+    publisher = "MicrosoftWindowsServer"
+    sku       = "2016-datacenter-gensecond"
+    version   = "latest"
+  }
+  timeouts {
+    create = "1h"
+    read = "1h"
+    update = "1h"
+    delete = "1h"
+  }
+  
+}
+resource "azurerm_virtual_machine_extension" "killspoke2vmfirewall" {
+  auto_upgrade_minor_version = true
+  name                       = "killspokevmfirewall"
+  publisher                  = "Microsoft.Compute"
+  type                       = "CustomScriptExtension"
+  type_handler_version       = "1.10"
+  virtual_machine_id         = azurerm_windows_virtual_machine.spoke2vm.id
+  settings = <<SETTINGS
+    {
+      "commandToExecute": "powershell -command \"Set-NetFirewallProfile -Enabled False\""
+    }
+  SETTINGS
+  timeouts {
+    create = "1h"
+    read = "1h"
+    update = "1h"
+    delete = "1h"
+  }
+  
+}
